f4cbd1e814c25a1b6f3d1b7b40fa3002278f68ef
m41en <male.b.t.2000@gmail.com>
2023-08-13 21:49:51 -0300

    Migraciones y modelos de API

---
+++ database/migrations/2023_08_13_234908_participants.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Participants extends Migration
+{
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id('id_participant');
+            $table->unsignedBigInteger('fk_id_event');
+            $table->unsignedBigInteger('fk_id_user');
+
+            $table->foreign('fk_id_event')->references('id_event')->on('events');
+            $table->foreign('fk_id_user')->references('id')->on('users');
+
+            $table->unique(['fk_id_event','fk_id_user']);
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down()
+    {
+        
+    }
+}
