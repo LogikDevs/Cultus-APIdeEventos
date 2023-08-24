a77ba491470f0b52c4162070cd6f84911f292362
m41en <male.b.t.2000@gmail.com>
2023-08-24 00:18:18 -0300

    Rutas a funciones necesarias para la creacion de un evento

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/events/create', [EventsController::class, 'CreateEvent']);
+Route::post('/event/interests/create', [EventInterestsController::class, 'CreateEventInterests']);
+
