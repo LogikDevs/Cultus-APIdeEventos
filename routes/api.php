97349b185462aedf47b1d94b5b0122254fdf863d
m41en <male.b.t.2000@gmail.com>
2023-08-26 00:17:38 -0300

    Seguir eventos, asignar rol default al seguir, dejar de seguir

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventsController;
 use App\Http\Controllers\EventInterestsController;
+use App\Http\Controllers\ParticipantsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,5 +24,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/events', [EventsController::class, 'List']);
 Route::post('/events/create', [EventsController::class, 'CreateEvent']);
-Route::post('/event/interests/create', [EventInterestsController::class, 'CreateEventInterests']);
+Route::post('/events/interests/create', [EventInterestsController::class, 'CreateEventInterests']);
+
+Route::get('/events/participants', [ParticipantsController::class, 'List']);
+Route::post('/events/participants/create', [ParticipantsController::class, 'CreateParticipant']);
+Route::post('/events/participants/delete/{id}', [ParticipantsController::class, 'UnParticipate']);
+
 
