74a96433a0ca6b044b075ef9d8f21fde4c8b8e5e
m41en <male.b.t.2000@gmail.com>
2023-08-31 20:18:33 -0300

    Autenticacion desde API de eventos a API de autenticacion correcto

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventsController;
 use App\Http\Controllers\EventInterestsController;
 use App\Http\Controllers\ParticipantsController;
+use App\Http\Middleware\Autenticacion;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,15 +22,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::prefix('v1')->middleware(Autenticacion::class)->group(function(){
+    Route::get('/events', [EventsController::class, 'List']);
+    Route::get('/events/interested/{id}', [EventsController::class, 'ListInterested']);
+    Route::get('/events/followed/{id}', [EventsController::class, 'ListFollowed']);
+    Route::post('/events/create', [EventsController::class, 'CreateEvent']);
+    Route::post('/events/interests/create', [EventInterestsController::class, 'CreateEventInterests']);
 
-Route::get('/events', [EventsController::class, 'List']);
-Route::get('/events/interested/{id}', [EventsController::class, 'ListInterested']);
-Route::get('/events/followed/{id}', [EventsController::class, 'ListFollowed']);
-Route::post('/events/create', [EventsController::class, 'CreateEvent']);
-Route::post('/events/interests/create', [EventInterestsController::class, 'CreateEventInterests']);
-
-Route::get('/events/participants', [ParticipantsController::class, 'List']);
-Route::post('/events/participants/create', [ParticipantsController::class, 'CreateParticipant']);
-Route::post('/events/participants/delete/{id}', [ParticipantsController::class, 'UnParticipate']);
-
-
+    Route::get('/events/participants', [ParticipantsController::class, 'List']);
+    Route::post('/events/participants/create', [ParticipantsController::class, 'CreateParticipant']);
+    Route::post('/events/participants/delete/{id}', [ParticipantsController::class, 'UnParticipate']);
+});
