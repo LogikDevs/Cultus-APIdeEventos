192728adae03e6d65c0e0b5223268ee150338b13
m41en <male.b.t.2000@gmail.com>
2023-09-12 23:56:49 -0300

    Ver participantes de un evento

--- routes/api.php
+++ routes/api.php
@@ -24,12 +24,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('v1')->middleware(Autenticacion::class)->group(function(){
     Route::get('/events', [EventsController::class, 'List']);
-    Route::get('/events/interested/{id}', [EventsController::class, 'ListInterested']);
-    Route::get('/events/followed/{id}', [EventsController::class, 'ListFollowed']);
-    Route::post('/events/create', [EventsController::class, 'CreateEvent']);
-    Route::post('/events/interests/create', [EventInterestsController::class, 'CreateEventInterests']);
+    Route::get('/event/interested/{id}', [EventsController::class, 'ListInterested']);
+    Route::get('/event/followed/{id}', [EventsController::class, 'ListFollowed']);
+    Route::post('/event/create', [EventsController::class, 'CreateEvent']);
+    Route::post('/event/interests/create', [EventInterestsController::class, 'CreateEventInterests']);
 
     Route::get('/events/participants', [ParticipantsController::class, 'List']);
+    Route::get('/event/participants/{id_event}', [ParticipantsController::class, 'ListParticipants']);
     Route::post('/events/participants/create', [ParticipantsController::class, 'CreateParticipant']);
     Route::post('/events/participants/delete/{id}', [ParticipantsController::class, 'UnParticipate']);
 });
