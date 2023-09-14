35232d11a951a5328024292d79240f316b9d067d
m41en <male.b.t.2000@gmail.com>
2023-09-14 01:37:57 -0300

    Seguir y dejar de seguir evento con bearer + arreglo de rutas + funcion para tomar id de usuario (clean code)

--- app/Http/Controllers/ParticipantsController.php
+++ app/Http/Controllers/ParticipantsController.php
@@ -7,12 +7,19 @@ use App\Models\Events;
 use App\Models\Participants;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 
 class ParticipantsController extends Controller
 {
     public function List(Request $request) {
         return Participants::all();
+    }
+    
+    public function GetUserId(Request $request) {
+        $tokenHeader = [ "Authorization" => $request -> header("Authorization")];
+        $response = Http::withHeaders($tokenHeader)->get(getenv("API_AUTH_URL") . "/api/v1/validate");
+        return $response['id'];
     }
     
     public function ListParticipants($id_event) {
@@ -35,24 +42,24 @@ class ParticipantsController extends Controller
     }
 
     public function CreateParticipant(Request $request) {
+        $id_user = $this->GetUserId($request);
+
         $validator = Validator::make($request->all(), [
-            'fk_id_event' => 'required | exists:events,id',
-            'fk_id_user' => 'required | exists:users,id',
+            'fk_id_event' => 'required | exists:events,id'
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
 
-        return $this->GetNewParticipant($request);
+        return $this->GetNewParticipant($request, $id_user);
     }
 
-    public function GetNewParticipant(request $request) {
+    public function GetNewParticipant(request $request, $id_user) {
         $fk_id_event = $request->input('fk_id_event');
-        $fk_id_user = $request->input('fk_id_user');
 
         $existingParticipant = Participants::where('fk_id_event', $fk_id_event)
-            ->where('fk_id_user', $fk_id_user)
+            ->where('fk_id_user', $id_user)
             ->withTrashed()
             ->first();
 
@@ -60,22 +67,24 @@ class ParticipantsController extends Controller
             return response()->json(['error' => 'Este usuario ya forma parte de este evento'], 400);
         }
 
-        return $this->SaveParticipant($fk_id_event, $fk_id_user);
+        return $this->SaveParticipant($fk_id_event, $id_user);
     }
 
-    public function SaveParticipant($fk_id_event, $fk_id_user) {
+    public function SaveParticipant($fk_id_event, $id_user) {
         $newParticipant = new Participants();
         $newParticipant -> fk_id_event = $fk_id_event;
-        $newParticipant -> fk_id_user = $fk_id_user;
+        $newParticipant -> fk_id_user = $id_user;
         $newParticipant -> rol = 'follower';
         $newParticipant -> save();
         return $newParticipant;
     }
 
-    public function UnParticipate($id_participates) {
-        $participant = Participants::find($id_participates);
+    public function UnParticipate(Request $request) {
+        $id_user = $this->GetUserId($request);
+
+        $participant = Participants::find($id_user);
         if (!$participant) {
-            return response()->json(['error' => 'No existe.'], 404);
+            return response()->json(['error' => 'El usuario no forma parte de este evento.'], 404);
         }
 
         $participant->delete();
