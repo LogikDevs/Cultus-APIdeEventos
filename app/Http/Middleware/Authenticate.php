f3e18d6f66c393eca2e4d4aee39089b911db3845
m41en <male.b.t.2000@gmail.com>
2023-09-14 00:28:16 -0300

    Modificacion de archivos para autenticacion

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Middleware;
-
+/*
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -11,11 +11,33 @@ class Authenticate extends Middleware
      *
      * @param  \Illuminate\Http\Request  $request
      * @return string|null
-     */
+     /
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
             return route('login');
         }
+    }
+}
+*/
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class Authenticate
+{
+    public function handle(Request $request, Closure $next)
+    {
+        //return $next($request);
+        $tokenHeader = [ "Authorization" => $request -> header("Authorization")];
+        
+        $response = Http::withHeaders($tokenHeader)->get(getenv("API_AUTH_URL") . "/api/v1/validate");
+
+        
+        if($response -> successful())
+            return $next($request);
+        
+        return response(['message' => 'Not Allowed'], 403);
     }
 }
