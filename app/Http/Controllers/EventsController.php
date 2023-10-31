50255b33a3f339216fe1b062faf630e42eab9ef2
m41en <male.b.t.2000@gmail.com>
2023-10-31 19:58:27 -0300

    Listar participantes y sus respectivos roles de cada evento

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -61,9 +61,10 @@ class EventsController extends Controller
         $event = $this->GetEvent($eventId);
         $event['admin'] = $this->GetAdmin($event[0]['id']);
         $event['interests'] = $this->GetInterestsFromEvent($event[0]['id'], $tokenHeader);
+        $event['participants'] = $this->GetEventParticipants($event[0]['id']);
         $event['updates'] = $this->GetEventUpdates($event[0]['id'], $tokenHeader)->json();
         
-            return $event;
+        return $event;
     }
 
     public function GetEvent($eventId) {
@@ -101,6 +102,21 @@ class EventsController extends Controller
         }
 
         return $int;
+    }
+
+    public function GetEventParticipants($eventId) {
+        $participants = Participants::where('fk_id_event', $eventId)->get();
+        $participantData = [];
+
+        foreach ($participants as $participant) {
+            $participantData[] = [
+                'name' => $participant->user->name,
+                'surname' => $participant->user->surname,
+                'rol' => $participant->rol,
+            ];
+        }
+
+        return $participantData;
     }
 
     public function GetEventUpdates ($fk_id_event, $tokenHeader) {
